17147d6723746dfb6149137dd53f390869311b75
GokuJeans <luochinne@gmail.com>
2025-02-27 14:49:41 -0700

    Chpt 5 PA TU with modifications

--- queueAsLinkedList.c
+++ queueAsLinkedList.c
@@ -53,7 +53,7 @@ void enqueue(Queue* queue, int item) {
 int dequeue(Queue* queue) {
     if (queue->front == NULL) {
         // Return 0 if the queue is empty
-        printf("An item was not dequeued.\n");
+        printf("An item was not dequeued.\n"); //Displays that the item was not removed 
 		return 0;
     }
     // Save the data from the front node and move the front pointer
@@ -65,48 +65,48 @@ int dequeue(Queue* queue) {
         queue->rear = NULL;
     }
     free(temp); // free memory for the dequeued node
-    printf("Item %d was removed from the queue.\n", dequeuedItem); 
+    printf("Item %d was removed from the queue.\n", dequeuedItem); // Displays the item that was removed
 }
 
 // Function to check if the queue is empty
 int isEmpty(Queue* queue) {
-    if (queue->front == NULL) {
+    if (queue->front == NULL) { //Looks at the queue front and if there isn't anything declares that the queue is empty
 		printf("The queue is empty.\n");
 	}
-	else {
+	else { // Else, the queue is not empty if it is not null
 		printf("The queue is not empty.\n");
 	}
 }
 
 // Function to peek at the front item without removing it
 int peek(Queue* queue) {
-    if (queue->front != NULL) {
+    if (queue->front != NULL) { // If the front of the queue is not null, displays the data of the front of the queue's data
 		printf("The top item on the queue is: %d\n", queue->front->data);
 	}
 	else {
-		printf("The queue is empty.\n");
+		printf("The queue is empty.\n"); // If the queue is empty, declares that the queue is empty
 	}
 }
 
 // Function to get the length of the queue
 int getLength(Queue* queue) {
-	int length= 0;
-    Node* curNode = queue->front;
-	while (curNode != NULL) {
-		length += 1;
-		curNode = curNode->next;
+	int length = 0; // declares and initializes the variable length at 0
+    Node* curNode = queue->front; // puts the current node as the first in the queue 
+	while (curNode != NULL) { // while there are still nodes to count 
+		length += 1; // adds 1 to length 
+		curNode = curNode->next; // moves onto the next node 
 	}
-	printf("The queue's length is: %d\n", length);
+	printf("The queue's length is: %d\n", length); // when it's done, will display what the length of the queue is 
 }
 
 // Function to traverse and display the queue
 void displayQueue(Queue* queue) {
-    Node* curNode = queue->front;
-	while (curNode != NULL) {
-		printf("%d -> ", curNode->data);
-		curNode = curNode->next;
+    Node* curNode = queue->front; // starts from the beginning of the queue
+	while (curNode != NULL) { // while there are still nodes in the queue
+		printf("%d -> ", curNode->data); // prints the data of the current node
+		curNode = curNode->next; // moves onto the next node 
 	}
-	printf("NULL\n");
+	printf("NULL\n"); // if it is null, will display null 
 }
 
 int main() {
